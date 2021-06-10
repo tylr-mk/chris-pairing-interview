a0cfd5a27abff6ae6e61602b60b93a44d05aea1c
Mike Taylor <tylr.mk@gmail.com>
2021-06-10 12:02:20 +0100

    Add base main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "google" {
+  project = "datatech-pairing-test"
+  region = "europe-west2"
+  zone = "europe-west2-c"
+}
+
+provider "google-beta" {
+  project = "datatech-pairing-test"
+  region = "europe-west2"
+  zone = "europe-west2-c"
+}
+
